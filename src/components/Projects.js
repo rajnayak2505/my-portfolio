0906dd63e56579574650ab639619dc07ca671607
Rajesh Nayak <Rajesh.Nayak@mastercard.com>
2025-02-06 20:23:18 +0530

    Projects component

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,8 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {Reactprojects,JSprojects, CSSprojects} from '../utils/constants'
+import { Link } from 'react-router-dom';
+
 
 const Projects = () => {
+
   return (
-    <div>Projects</div>
+    <div className='projects'>
+      <h1>ReactJS Projects</h1>
+      <ul>
+        {Reactprojects.map((project) => 
+          <li>
+            <img alt='project img' src={project.img}/>
+            <h2>{project.name}</h2>
+            <Link target="_blank" to={project.github}>GitHub Repo.</Link>
+            <Link target="_blank" to={project.livePreview}>Preview</Link>
+          </li>
+        )}
+      </ul>
+      <h1>JavaScript Projects</h1>
+      <ul>
+        {JSprojects.map((project) => 
+          <li>
+            <img alt='project img' src={project.img}/>
+            <h2>{project.name}</h2>
+            <Link target="_blank" to={project.github}>GitHub Repo.</Link>
+            <Link target="_blank" to={project.livePreview}>Preview</Link>
+          </li>
+        )}
+      </ul>
+      <h1>CSS Projects</h1>
+      <ul>
+        {CSSprojects.map((project) => 
+          <li>
+            <img alt='project img' src={project.img}/>
+            <h2>{project.name}</h2>
+            <Link target="_blank" to={project.github}>GitHub Repo.</Link>
+            <Link target="_blank" to={project.livePreview}>Preview</Link>
+          </li>
+        )}
+      </ul>
+    </div>
   )
 }
 
---
+++ src/utils/constants.js
@@ -0,0 +1,106 @@
+import RJ1 from "../images/RJ1.png"
+import RJ2 from "../images/RJ2.png"
+import JS1 from "../images/JS1.png"
+import JS2 from "../images/JS2.png"
+import JS3 from "../images/JS3.png"
+import css1 from "../images/css1.png"
+import css2 from "../images/css2.png"
+import css3 from "../images/css3.png"
+import css4 from "../images/css4.png"
+
+export const Reactprojects = [
+    {
+        id:1,
+        name:"Recipe App",
+        decription:null,
+        github:"https://github.com/rajnayak2505/react-recipe-app",
+        livePreview:"https://staging-recipe.netlify.app/",
+        img:RJ1
+    },
+    {
+        id:2,
+        name:"Netflix GPT",
+        decription:null,
+        github:"https://github.com/rajnayak2505/netflix-gpt",
+        livePreview:"https://grand-daifuku-f22855.netlify.app/",
+        img:RJ2
+    },
+    {
+        id:3,
+        name:"My Youtube [Inprpgress]",
+        decription:null,
+        github:"https://github.com/rajnayak2505/my-youtube",
+        livePreview:"",
+        img:""
+    },
+    {
+        id:4,
+        name:"Food App [Inprpgress]",
+        decription:null,
+        github:"https://github.com/rajnayak2505/Namaste-React",
+        livePreview:"",
+        img:""
+    }
+];
+
+export const JSprojects = [
+    {
+        id:1,
+        name:"Color Flipper",
+        decription:null,
+        github:"https://github.com/rajnayak2505/Color-Flipper",
+        livePreview:"https://rajnayak2505.github.io/Color-Flipper/",
+        img:JS1
+    },
+    {
+        id:2,
+        name:"Simple Counter",
+        decription:null,
+        github:"https://github.com/rajnayak2505/Color-Flipper",
+        livePreview:"https://rajnayak2505.github.io/Color-Flipper/",
+        img:JS2
+    },
+    {
+        id:3,
+        name:"Slider Preview",
+        decription:null,
+        github:"https://github.com/rajnayak2505/preview-review",
+        livePreview:"https://rajnayak2505.github.io/preview-review/",
+        img:JS3
+    },
+]
+
+export const CSSprojects = [
+    {
+        id:1,
+        name:"Digital",
+        decription:null,
+        github:"https://github.com/rajnayak2505/digital-template",
+        livePreview:"https://rajnayak2505.github.io/digital-template/",
+        img:css1
+    },
+    {
+        id:2,
+        name:"Photographer Hero Page",
+        decription:null,
+        github:"https://github.com/rajnayak2505/photographer-ui-design",
+        livePreview:"https://rajnayak2505.github.io/photographer-ui-design/",
+        img:css2
+    },
+    {
+        id:3,
+        name:"Doctor Portal",
+        decription:null,
+        github:"https://github.com/rajnayak2505/doctors",
+        livePreview:"https://rajnayak2505.github.io/doctors/",
+        img:css3
+    },
+    {
+        id:4,
+        name:"MI Clone",
+        decription:null,
+        github:"https://github.com/rajnayak2505/miWebsite.github.io",
+        livePreview:"https://rajnayak2505.github.io/miWebsite.github.io/",
+        img:css4
+    }
+]
